fa9153c9587f5bf2aae19664b9b140a455eeb1f5
Alfredmalm <alfredma@kth.se>
2026-04-02 13:04:11 +0200

    updated to the new input system

--- My project/Assets/Ghost_movement.cs
+++ My project/Assets/Ghost_movement.cs
@@ -1,36 +1,111 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Ghost_movement : MonoBehaviour
 {
-    public float speed = 5f;
-    public Player player;
+    public InputActionAsset inputActions;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction lookAction;
+    private InputAction crouchAction;
+
+    private Vector2 moveVector;
+    private Vector2 lookVector;
+
+    private Rigidbody rb;
+    private Animator animator;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float rotationSpeed = 100f;
+
+    private InputActionMap playerMap;
+    private bool isActive = true;
+
+    private void Awake()
     {
+        playerMap = inputActions.FindActionMap("Player");
 
+        moveAction = playerMap.FindAction("Move");
+        jumpAction = playerMap.FindAction("Jump");
+        lookAction = playerMap.FindAction("Look");
+        crouchAction = playerMap.FindAction("Crouch");
+
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
- 
-
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        if(player.activeplayer == this.transform){  
-            if (Input.GetKey(KeyCode.W))
-                transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            if (Input.GetKey(KeyCode.S))
-                transform.Translate(Vector3.back * Time.deltaTime * speed);
-            if (Input.GetKey(KeyCode.A))
-                transform.Translate(Vector3.left * Time.deltaTime * speed);
-            if (Input.GetKey(KeyCode.D))
-                transform.Translate(Vector3.right * Time.deltaTime * speed);
-            if (Input.GetKey(KeyCode.Space))
-                transform.Translate(Vector3.up * Time.deltaTime * speed);
-            if (Input.GetKey(KeyCode.LeftShift))
-                transform.Translate(Vector3.down * Time.deltaTime * speed);
-            }
+        playerMap.Enable();
     }
-        
+
+    private void OnDisable()
+    {
+        playerMap.Disable();
+    }
+
+    private void Update()
+    {
+        if (!isActive) return;
+
+        moveVector = moveAction.ReadValue<Vector2>();
+        lookVector = lookAction.ReadValue<Vector2>();
+
+        if (jumpAction.IsPressed())
+        {
+            Jump();
+        }
+        if (crouchAction.IsPressed()){
+            Crouch();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isActive) return;
+
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        Vector3 move = (transform.forward * moveVector.y + transform.right * moveVector.x) * moveSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + move);
+
+        animator.SetFloat("Speed", moveVector.magnitude); // better value
+    }
+
+    private void Rotate()
+    {
+        float rotation = lookVector.x * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turn = Quaternion.Euler(0, rotation, 0);
+
+        rb.MoveRotation(rb.rotation * turn);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce);
+        animator.SetTrigger("Jump");
+    }
+
+    private void Crouch()
+    {
+        rb.AddForce(Vector3.down * jumpForce);
+        animator.SetTrigger("Jump");
+        animator.SetBool("IsCrouching", true);
+    }
+
+    public void Active(bool active)
+    {
+        isActive = active;
+        if (active)
+            playerMap.Enable();
+        else
+            playerMap.Disable();
+    }
 }
-
