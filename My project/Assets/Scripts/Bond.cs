d9e0be203ced572e5f83760cd199e504f0cb01ed
Anna Likhanova <annet.likhanova@gmail.com>
2026-04-03 13:19:16 +0200

    feat: add Bond between two characters Add bond between ghost and dog via line renderer, organize Scripts

---
+++ My project/Assets/Scripts/Bond.cs
@@ -0,0 +1,22 @@
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+
+public class Bond : MonoBehaviour
+{   
+    public LineRenderer bond;
+    public GameObject character1;
+    public GameObject character2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bond.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bond.SetPosition(0, character1.transform.position);
+        bond.SetPosition(1, character2.transform.position);
+
+    }
+}
